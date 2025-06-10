1803422eb61e980a0599b7681b86aa4c06a525eb
= <guerrajesusm72@gmail.com>
2025-06-09 20:44:29 -0700

    fix

--- src/components/GameCompleteModal.jsx
+++ src/components/GameCompleteModal.jsx
@@ -8,7 +8,8 @@ function GameCompleteModal({ isVisible, isWinner, onRestart, onBack, secretSong 
         <div className="bg-black/90 rounded-xl  gap-2 p-4 w-full sm:w-md flex flex-col items-center">
             <h3 className="text-white font-roboto text-xl font-semibold">
                 {isWinner ? "Felicidades acertaste!" : "Sigue intentando :("}
-                {!isWinner && "La canción era: " + secretSong?.title } 
+                <br />
+                {!isWinner && "La canción era: " + secretSong?.name } 
             </h3>
             <p className="text-white">¿Qué deseas hacer ahora?</p>
             <div className="flex gap-6 pt-4 w-full">
